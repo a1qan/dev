6396fe785df7ca870f9fb030845cd52a9aeb7d6a
Aidan <147556918+a1qan@users.noreply.github.com>
2026-05-18 23:36:07 +0100

    Revise aboutMe and project information
    Updated personal information and project details in config.

--- src/config.ts
+++ src/config.ts
@@ -9,15 +9,15 @@ export const siteConfig = {
     github: "https://github.com/ABDProjects",
   },
   aboutMe:
-    "I am a 14 year old Graphics and Web/Software Designer! I do small projects for fun and for myself, using my couple years of experience to make the best i can.",
+    "I am a 15 year old Graphics and Web/Software Designer! I do small projects with the help of vibecoding (with modifications) for fun and for myself, using my couple years of experience to make the best i can.",
   skills: ["HTML", "CSS", "Photoshop", "Figma", "FL Studio", "JS"],
   projects: [
     {
-      name: "2ManyTabs",
+      name: "2Many",
       description:
-        "2ManyTabs is a browser extension designed to make web browsing faster, easier, and more organized. It enhances your start page by adding a quick shortcuts feature & a multi-search engine.",
-      link: "https://2MT.aidanbray.com",
-      skills: ["HTML", "CSS", "JS"],
+        "2Many is a gathering of services that will help with your everyday life & workflow. Minimalistic and fast.",
+      link: "https://2many.org",
+      skills: ["HTML", "CSS", "JS", "Github" "More..."],
     },
     {
       name: "UM?",
